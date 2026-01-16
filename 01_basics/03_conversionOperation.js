9bf749a01e088b28aed8e3c8a9a688ffc0f09904
Utkarsh Srivastava <143518788+Utkarsh-Srivastava29@users.noreply.github.com>
2026-01-16 18:49:01 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,38 @@
+// let score="33abc"
+// let score=null
+// let score=undefined
+// let score=true
+// let score="hitesh"
+// console.log(typeof score);
+// console.log(typeof(score));
+// let ValueInNumber=Number(score)//yahan ye capital mein hai kyunki class based hai na isliye aise case hai upar mein 
+// console.log(typeof ValueInNumber)
+// console.log(ValueInNumber);
+
+
+//"33"=>33
+//"33abc"=>NaN
+//true=>1
+//false=>0
+
+// let isLoggedIn=1
+// let isLoggedIn=""
+let isLoggedIn="hitesh"
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+//1=>true
+//0=>false
+//""=>false
+//"hiesh"=>true
+
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log(typeof stringNumber)
+
+
+
+
+
+ 
