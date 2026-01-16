5d49b3acf2ed72e15e7364ff9e3013a36f0996b6
Utkarsh Srivastava <143518788+Utkarsh-Srivastava29@users.noreply.github.com>
2026-01-16 18:29:32 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,41 @@
+"use strict";//treat all the JS code as the newer version
+
+
+//we cannot use the statement below becuase most of the people who will teach us will teach us in the html or the developer console
+//kyunki javascript ka engine browser ke anar chhupa hua hai
+// alert(3+3);//we are using node.js and not the browser
+//we avoid semicolons in most of the scenarios
+
+
+
+console.log(3+3)//code readibility should be very high so we shoud not just give irrelevant indenation in the above and that is it about the above
+console.log("hitesh")
+
+
+
+let name ="hitesh"
+let age=18
+let isLoggedIn=false
+let state=null//we can give a single empty value to the above
+//ab maan lo hum kuch temperature dena chah rhe hain lekin server agr hamein zero bhej dega to wo to ek temperature and we cannot send that temperature in the system because it cannot send the temperature
+
+
+
+
+
+//number=>2 to power 53
+//if my number is large enough then we can go wih bigint
+//string==>double quote ""
+//boolean=>true/false
+//null==>js mein ek datattype hai lekin standalone value bhi hai ye above waali cheezein
+//undefined
+//symbol=>unique
+
+
+//object 
+console.log(typeof "hitesh");
+console.log(typeof null);//object
+console.log(typeof undefined);//undefined
+
+//null ka type object hai->very important interview question
+
